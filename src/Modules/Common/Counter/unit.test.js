88733845da12d808cef2e80947ac2a6d2eb7c716
Mark Samios <rhabola22@gmail.com>
2022-02-07 00:23:43 +0000

    made unit tests in the counter suite asynchronous

--- src/Modules/Common/Counter/unit.test.js
+++ src/Modules/Common/Counter/unit.test.js
@@ -1,4 +1,3 @@
-//import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect'
 import {render, fireEvent, waitFor, cleanup, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
@@ -8,36 +7,48 @@ import '^/globals.js'
 import Counter from '%/Common/Counter'
 
 describe('Counter Tests', () => {
-  it('<Counter /> Initial', () => {
-    //const {debug, getByText} = render(<Counter range={[10, 100]} step={1} />);
-    const {debug, getByText} = render(<Counter range={[10, 100]} step={1} />);
+  test('<Counter /> Initial', async () => {
+    const cStringsMock = jest.fn().mockReturnValue({
+      up: "+",
+      down: "-",
+      reset: "Reset",
+    });
 
-    waitFor(() => {
-      //debug()
-      expect(getByText(/Min:10/i)).toBeInTheDocument();
-      expect(getByText('10')).toBeInTheDocument();
-      expect(getByText('100')).toBeInTheDocument();
+    const controllerMock = jest.fn().mockImplementation(() => ({
+      min: 10, max: 100, step: 1,
+      onStepUp: ()=>{}, onStepDown: ()=>{}, onReset: ()=>{},
+      isUpDisabled: false, isDownDisabled: true, cStep: 1,
+    }));
+
+    const {debug, getByText} = render(<Counter range={[10, 100]} step={1} using={controllerMock} cStrings={cStringsMock()} />);
+
+    await waitFor(() => {
+      //debug();
       expect(getByText('-').closest('button')).toHaveAttribute('disabled');
-      expect(getByText('+').closest('button')).toHaveAttribute('enabled');
+      expect(getByText('+').closest('button')).not.toHaveAttribute('disabled');
       expect(getByText(/reset/i).closest('button')).toHaveAttribute('disabled');
     })
   });
 
-  test("<Counter /> Increment", () => {
+  test("<Counter /> Increment", async () => {
     const {getByText} = render(<Counter range={[10, 100]} step={5} />);
     fireEvent.click(getByText('+'))
-    waitFor(() => {
+    await waitFor(() => {
       expect(getByText('15')).toBeInTheDocument();
+    });
+    fireEvent.click(getByText('+'))
+    await waitFor(() => {
+      expect(getByText('20')).toBeInTheDocument();
     });
   });
 
-  it("<Counter /> Decrement", () => {
+  test("<Counter /> Decrement", async () => {
     const {getByText} = render(<Counter range={[10, 100]} step={1} />);
     fireEvent.click(getByText('+'))
     fireEvent.click(getByText('+'))
     fireEvent.click(getByText('+'))
     fireEvent.click(getByText('-'))
-    waitFor(() => {
+    await waitFor(() => {
       expect(getByText('12')).toBeInTheDocument();
     });
   })
@@ -50,7 +61,7 @@ describe('Counter Tests', () => {
       expect(getByText('12')).toBeInTheDocument();
     });
     fireEvent.click(getByText(/reset/i))
-    waitFor(() => {
+    await waitFor(() => {
       expect(getByText('10')).toBeInTheDocument();
     });
   });
